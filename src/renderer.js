5074d871de36b7b711f8ae61bf1bcc849d4035d1
Jackson <meye4606@stthomas.edu>
2023-05-07 20:38:57 -0500

    Add custom house, different scenes, light translating

--- src/renderer.js
+++ src/renderer.js
@@ -107,17 +107,354 @@ class Renderer {
         box.material = materials['illum_' + this.shading_alg];
         current_scene.models.push(box);
 
+        //translate lights
+        scene.onKeyboardObservable.add((keypress) => {
+            if (keypress.type === KeyboardEventTypes.KEYDOWN) {
+                switch (keypress.event.key) {
+                    case 'a':
+                    current_scene.lights[this.active_light].position.x -= 1;
+                    break;
+                    case 'd':
+                    current_scene.lights[this.active_light].position.x += 1;
+                    break;
+                    case 'f':
+                    current_scene.lights[this.active_light].position.y -= 1;
+                    break;
+                    case 'r':
+                    current_scene.lights[this.active_light].position.y += 1;
+                    break;
+                    case 'w':
+                    current_scene.lights[this.active_light].position.z -= 1;
+                    break;
+                    case 's':
+                    current_scene.lights[this.active_light].position.z += 1;
+                    break;
+                }
+            }
+        });
 
         // Animation function - called before each frame gets rendered
         scene.onBeforeRenderObservable.add(() => {
             // update models and lights here (if needed)
             // ...
 
+
             // update uniforms in shader programs
             this.updateShaderUniforms(scene_idx, materials['illum_' + this.shading_alg]);
             this.updateShaderUniforms(scene_idx, materials['ground_' + this.shading_alg]);
         });
     }
+
+    createScene1(scene_idx) {
+        let current_scene = this.scenes[scene_idx];
+        let scene = current_scene.scene;
+        let materials = current_scene.materials;
+        let ground_mesh = current_scene.ground_mesh;
+
+        // Set scene-wide / environment values
+        scene.clearColor = current_scene.background_color;
+        scene.ambientColor = current_scene.ambient;
+        scene.useRightHandedSystem = true;
+
+        // Create camera
+        current_scene.camera = new UniversalCamera('camera', new Vector3(0.0, 1.8, 10.0), scene);
+        current_scene.camera.setTarget(new Vector3(0.0, 1.8, 0.0));
+        current_scene.camera.upVector = new Vector3(0.0, 1.0, 0.0);
+        current_scene.camera.attachControl(this.canvas, true);
+        current_scene.camera.fov = 35.0 * (Math.PI / 180);
+        current_scene.camera.minZ = 0.1;
+        current_scene.camera.maxZ = 100.0;
+
+        // Create point light sources
+        let light0 = new PointLight('light0', new Vector3(1.0, 1.0, 5.0), scene);
+        light0.diffuse = new Color3(1.0, 1.0, 1.0);
+        light0.specular = new Color3(1.0, 1.0, 1.0);
+        current_scene.lights.push(light0);
+
+        let light1 = new PointLight('light1', new Vector3(0.0, 3.0, 0.0), scene);
+        light1.diffuse = new Color3(1.0, 1.0, 1.0);
+        light1.specular = new Color3(1.0, 1.0, 1.0);
+        current_scene.lights.push(light1);
+
+        // Create ground mesh
+        let white_texture = RawTexture.CreateRGBTexture(new Uint8Array([255, 255, 255]), 1, 1, scene);
+        let ground_heightmap = new Texture('/heightmaps/default.png', scene);
+        ground_mesh.scaling = new Vector3(20.0, 1.0, 20.0);
+        ground_mesh.metadata = {
+            mat_color: new Color3(0.10, 0.65, 0.15),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.0, 0.0, 0.0),
+            mat_shininess: 1,
+            texture_scale: new Vector2(1.0, 1.0),
+            height_scalar: 1.0,
+            heightmap: ground_heightmap
+        }
+        ground_mesh.material = materials['ground_' + this.shading_alg];
+
+        // Create other models
+        // Our custom house
+        let house = new Mesh("house", scene);
+        let vertexData = new VertexData();
+        let positions = [
+            6, 0, 2,
+            4, 0, 2,
+            4, 0, -2,
+            6, 0, -2,
+            6, 0.5, 2,
+            4, 0.5, 2,
+            4, 0.5, -2,
+            6, 0.5, -2,
+            5, 1, 2,
+            5, 1, -2,
+            3.8, 0.5, 2,
+            3.8, 0.5, -2,
+            6.2, 0.5, 2,
+            6.2, 0.5, -2
+        ];
+        let indices = [
+            0, 1, 2,
+            2, 3, 0,
+            0, 1, 4,
+            1, 4, 5,
+            1, 2, 5,
+            5, 6, 2,
+            2, 3, 6,
+            3, 6, 7,
+            0, 3, 4,
+            3, 4, 7,
+            8, 9, 10,
+            9, 10, 11,
+            8, 9, 12,
+            9, 12, 13,
+            5, 6, 10,
+            6, 10, 11,
+            4, 7, 12,
+            7, 12, 13,
+            9, 11, 13,
+            8, 10, 12
+        ];
+        let normals = [];
+        VertexData.ComputeNormals(positions, indices, normals);
+
+        vertexData.positions = positions;
+        vertexData.indices = indices;
+        vertexData.normals = normals;
+        vertexData.applyToMesh(house);
+        house.metadata = {
+            mat_color: new Color3(1.0, 0.5, 3.0),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.8, 0.8, 0.8),
+            mat_shininess: 16,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        house.material = materials['illum_' + this.shading_alg];
+        current_scene.models.push(house);
+
+        //translate lights
+        scene.onKeyboardObservable.add((keypress) => {
+            if (keypress.type === KeyboardEventTypes.KEYDOWN) {
+                switch (keypress.event.key) {
+                    case 'a':
+                    current_scene.lights[this.active_light].position.x -= 1;
+                    break;
+                    case 'd':
+                    current_scene.lights[this.active_light].position.x += 1;
+                    break;
+                    case 'f':
+                    current_scene.lights[this.active_light].position.y -= 1;
+                    break;
+                    case 'r':
+                    current_scene.lights[this.active_light].position.y += 1;
+                    break;
+                    case 'w':
+                    current_scene.lights[this.active_light].position.z -= 1;
+                    break;
+                    case 's':
+                    current_scene.lights[this.active_light].position.z += 1;
+                    break;
+                }
+            }
+        });
+        // Animation function - called before each frame gets rendered
+        scene.onBeforeRenderObservable.add(() => {
+            // update models and lights here (if needed)
+            // ...
+
+
+            // update uniforms in shader programs
+            this.updateShaderUniforms(scene_idx, materials['illum_' + this.shading_alg]);
+            this.updateShaderUniforms(scene_idx, materials['ground_' + this.shading_alg]);
+        });
+
+    }
+
+    createScene2(scene_idx) {
+        let current_scene = this.scenes[scene_idx];
+        let scene = current_scene.scene;
+        let materials = current_scene.materials;
+        let ground_mesh = current_scene.ground_mesh;
+
+        // Set scene-wide / environment values
+        scene.clearColor = current_scene.background_color;
+        scene.ambientColor = current_scene.ambient;
+        scene.useRightHandedSystem = true;
+
+        // Create camera
+        current_scene.camera = new UniversalCamera('camera', new Vector3(0.0, 1.8, 10.0), scene);
+        current_scene.camera.setTarget(new Vector3(0.0, 1.8, 0.0));
+        current_scene.camera.upVector = new Vector3(0.0, 1.0, 0.0);
+        current_scene.camera.attachControl(this.canvas, true);
+        current_scene.camera.fov = 35.0 * (Math.PI / 180);
+        current_scene.camera.minZ = 0.1;
+        current_scene.camera.maxZ = 100.0;
+
+        // Create point light sources
+        let light0 = new PointLight('light0', new Vector3(1.0, 1.0, 5.0), scene);
+        light0.diffuse = new Color3(1.0, 1.0, 1.0);
+        light0.specular = new Color3(1.0, 1.0, 1.0);
+        current_scene.lights.push(light0);
+
+        let light1 = new PointLight('light1', new Vector3(0.0, 3.0, 0.0), scene);
+        light1.diffuse = new Color3(1.0, 1.0, 1.0);
+        light1.specular = new Color3(1.0, 1.0, 1.0);
+        current_scene.lights.push(light1);
+
+        // Create ground mesh
+        let white_texture = RawTexture.CreateRGBTexture(new Uint8Array([255, 255, 255]), 1, 1, scene);
+        let ground_heightmap = new Texture('/heightmaps/default.png', scene);
+        ground_mesh.scaling = new Vector3(20.0, 1.0, 20.0);
+        ground_mesh.metadata = {
+            mat_color: new Color3(0.10, 0.65, 0.15),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.0, 0.0, 0.0),
+            mat_shininess: 1,
+            texture_scale: new Vector2(1.0, 1.0),
+            height_scalar: 1.0,
+            heightmap: ground_heightmap
+        }
+        ground_mesh.material = materials['ground_' + this.shading_alg];
+
+        // Create other models
+        let sphere = CreateSphere('sphere', {segments: 32}, scene);
+        sphere.position = new Vector3(1.0, 0.5, 3.0);
+        sphere.metadata = {
+            mat_color: new Color3(0.10, 0.35, 0.88),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.8, 0.8, 0.8),
+            mat_shininess: 16,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        sphere.material = materials['illum_' + this.shading_alg];
+        current_scene.models.push(sphere);
+
+        let box = CreateBox('box', {width: 2, height: 1, depth: 1}, scene);
+        box.position = new Vector3(-1.0, 0.5, 2.0);
+        box.metadata = {
+            mat_color: new Color3(0.75, 0.15, 0.05),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.4, 0.4, 0.4),
+            mat_shininess: 4,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        box.material = materials['illum_' + this.shading_alg];
+        current_scene.models.push(box);
+
+        // Our custom house
+        let house = new Mesh("house", scene);
+        let vertexData = new VertexData();
+        let positions = [
+            6, 0, 2,
+            4, 0, 2,
+            4, 0, -2,
+            6, 0, -2,
+            6, 0.5, 2,
+            4, 0.5, 2,
+            4, 0.5, -2,
+            6, 0.5, -2,
+            5, 1, 2,
+            5, 1, -2,
+            3.8, 0.5, 2,
+            3.8, 0.5, -2,
+            6.2, 0.5, 2,
+            6.2, 0.5, -2
+        ];
+        let indices = [
+            0, 1, 2,
+            2, 3, 0,
+            0, 1, 4,
+            1, 4, 5,
+            1, 2, 5,
+            5, 6, 2,
+            2, 3, 6,
+            3, 6, 7,
+            0, 3, 4,
+            3, 4, 7,
+            8, 9, 10,
+            9, 10, 11,
+            8, 9, 12,
+            9, 12, 13,
+            5, 6, 10,
+            6, 10, 11,
+            4, 7, 12,
+            7, 12, 13,
+            9, 11, 13,
+            8, 10, 12
+        ];
+        let normals = [];
+        VertexData.ComputeNormals(positions, indices, normals);
+
+        vertexData.positions = positions;
+        vertexData.indices = indices;
+        vertexData.normals = normals;
+        vertexData.applyToMesh(house);
+        house.metadata = {
+            mat_color: new Color3(1.0, 1.0, 2.0),
+            mat_texture: white_texture,
+            mat_specular: new Color3(0.8, 0.8, 0.8),
+            mat_shininess: 16,
+            texture_scale: new Vector2(1.0, 1.0)
+        }
+        house.material = materials['illum_' + this.shading_alg];
+        current_scene.models.push(house);
+
+        //translate lights
+        scene.onKeyboardObservable.add((keypress) => {
+            if (keypress.type === KeyboardEventTypes.KEYDOWN) {
+                switch (keypress.event.key) {
+                    case 'a':
+                    current_scene.lights[this.active_light].position.x -= 1;
+                    break;
+                    case 'd':
+                    current_scene.lights[this.active_light].position.x += 1;
+                    break;
+                    case 'f':
+                    current_scene.lights[this.active_light].position.y -= 1;
+                    break;
+                    case 'r':
+                    current_scene.lights[this.active_light].position.y += 1;
+                    break;
+                    case 'w':
+                    current_scene.lights[this.active_light].position.z -= 1;
+                    break;
+                    case 's':
+                    current_scene.lights[this.active_light].position.z += 1;
+                    break;
+                }
+            }
+        });
+
+        // Animation function - called before each frame gets rendered
+        scene.onBeforeRenderObservable.add(() => {
+            // update models and lights here (if needed)
+            // ...
+
+
+            // update uniforms in shader programs
+            this.updateShaderUniforms(scene_idx, materials['illum_' + this.shading_alg]);
+            this.updateShaderUniforms(scene_idx, materials['ground_' + this.shading_alg]);
+        });
+    }
+
 
     updateShaderUniforms(scene_idx, shader) {
         let current_scene = this.scenes[scene_idx];
